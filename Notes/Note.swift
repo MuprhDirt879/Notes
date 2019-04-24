fbfc052823eb3364cff7f640559b3d58f6711916
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-04-24 14:06:27 -0400

    fixed everything

---
+++ Notes/Note.swift
@@ -0,0 +1,9 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Ryan Murphy on 4/24/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import Foundation
---
+++ Notes/NoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteTableViewCell.swift
+//  Notes
+//
+//  Created by Ryan Murphy on 4/24/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
